b09aa6522b752fbd98dd32925943edb0ae23e181
Coral Fussman <coralfussman@gmail.com>
2020-03-21 18:31:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,14 +11,23 @@ def directors_totals(nds)
 
 puts nds
 
-result = {}
-director_index = 0
-while director_index < result.count do
-  director = result[director_index]
-  result[director[:name]] = gross_for_director(director)
-  director_index += 1
-end
-result
+dir_index = 0
+ totals = {}
+
+ while dir_index < nds.length do
+   dir_name = nds[dir_index][:name]
+   totals[dir_name] = 0
+   movie_index = 0
+
+   while movie_index < nds[dir_index][:movies].length do
+     totals[dir_name] += nds[dir_index][:movies][movie_index][:worldwide_gross]
+     movie_index += 1
+   end
+
+   dir_index += 1
+ end
+
+ totals 
 
   result = {
     "Stephen Spielberg"=>1357566430,
