1f2150cd7a3a15017174a5beeb10720d888cdbb8
Coral Fussman <coralfussman@gmail.com>
2020-03-21 18:02:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,14 +9,53 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
 
-puts nds pp
+puts nds
 directors = directors_database
+
+    spielberg_total = 0
+  row_index = 0
+  while row_index < directors.length do
+    column_index = 0
+    while column_index < directors[row_index].length do
+      inner_len = directors[row_index][column_index].length
+      inner_index = 0
+      while inner_index < inner_len do
+
+      spielberg_total += directors[row_index][column_index][inner_index]['Stephen Spielberg'][:worldwide_gross]
+              inner_index += 1
+            end
+                column_index += 1
+
+              end
+              row_index += 1
+              end
+
+
+
+      russo_total = 0
+      row_index = 0
+            while row_index < directors.length do
+              column_index = 0
+              while column_index < directors[row_index].length do
+                inner_len = directors[row_index][column_index].length
+                inner_index = 0
+                while inner_index < inner_len do
+
+                russo_total += directors[row_index][column_index][inner_index]['Russo Brothers'][:worldwide_gross]
+                        inner_index += 1
+                      end
+                          column_index += 1
+
+                        end
+                        row_index += 1
+                        end
+
   result = {
 
   }
   #
 
-  
+
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
   # ...
